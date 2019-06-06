86a18a9e3b2d76046927fd0e943911e312bbbcc2
Louis Shi <Shilou2010@gmail.com>
2019-06-06 00:42:37 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(event) {
+  console.log(event.target);
+
+  let inBetween = false;
+
+  if (event.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+
+  lastChecked = this;
+}
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
